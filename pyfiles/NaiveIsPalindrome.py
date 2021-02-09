00661ba1e7f7af6c11f53e64852f037573b4e6d5
Zachary Shifrel <70245212+dataseyo@users.noreply.github.com>
2021-02-08 23:47:41 -0500

    Update NaiveIsPalindrome.py

--- pyfiles/NaiveIsPalindrome.py
+++ pyfiles/NaiveIsPalindrome.py
@@ -1,10 +1,10 @@
 class Solution(object):
     def isPalindrome(self, x):
-        str1 = str(x)
-        str2 = str1[::-1]
+        str1 = str(x) # change int to string
+        str2 = str1[::-1] # reverse string
         #index1 = x[0]
         #index2 = len(x) - 1
-        if str1 == str2:
+        if str1 == str2: # compare strings
             return True
         else: 
             return False
